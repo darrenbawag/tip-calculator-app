9b1285f1eece8c818ff7588e0677d13521f1842b
Darren Bawag <renash12@gmail.com>
2021-07-26 19:20:00 +0800

    Update Bill.js and People.js

--- src/components/Bill.js
+++ src/components/Bill.js
@@ -1,12 +1,14 @@
 const Bill = ({ bill, handleChangeBill, dollar }) => {
 	return (
 		<div className="input__wrap">
-			<label className="input__label">Bill</label>
+			<label className="input__label" htmlFor="bill">Bill</label>
 			<div className="input__group">
                 <img className="input__icon" src={dollar} alt="dollar sign" />
 				<input
 					className="input__text"
-					type="text"
+					type="number"
+					id="bill"
+					name="bill"
 					value={bill}
 					onChange={handleChangeBill}
 					placeholder="0"
--- src/components/People.js
+++ src/components/People.js
@@ -1,12 +1,14 @@
 const People = ({ numPeople, handleChangeNumPeople, person }) => {
 	return (
 		<div className="input__wrap">
-			<label className="input__label">Number of People</label>
+			<label className="input__label" htmlFor="people">Number of People</label>
 			<div className="input__group">
                 <img className="input__icon" src={person} alt="person sign" />
 				<input
 					className="input__text"
 					type="text"
+					id="people"
+					name="people"
 					value={numPeople}
 					onChange={handleChangeNumPeople}
 					placeholder="0"
